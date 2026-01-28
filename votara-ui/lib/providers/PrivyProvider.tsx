2f23ea917b7dd9df84f31f17a8f0cd8a189c0e34
althof3 <althoframdhan3@gmail.com>
2026-01-28 10:40:03 +0700

    add BE env

--- votara-ui/lib/providers/PrivyProvider.tsx
+++ votara-ui/lib/providers/PrivyProvider.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import { PrivyProvider as Privy } from '@privy-io/react-auth';
 import { base, baseSepolia } from 'viem/chains';
-import { WagmiProvider } from '@privy-io/wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { http } from 'viem';
 import { createConfig } from 'wagmi';
 
-const queryClient = new QueryClient();
 
 // Wagmi config for Privy
 export const wagmiConfig = createConfig({
@@ -17,28 +13,3 @@ export const wagmiConfig = createConfig({
     [baseSepolia.id]: http(),
   },
 });
-
-export function PrivyProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <Privy
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-      config={{
-        loginMethods: ['wallet', 'email', 'farcaster'],
-        appearance: {
-          theme: 'light',
-          accentColor: '#676FFF',
-        },
-        embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
-        },
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <WagmiProvider config={wagmiConfig}>
-          {children}
-        </WagmiProvider>
-      </QueryClientProvider>
-    </Privy>
-  );
-}
-
